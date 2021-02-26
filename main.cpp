844f5b72b7fec752830ceaf014379f133243e55d
TheTimBrick <tgh6407@gmail.com>
2021-02-26 01:05:15 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+void function() {
+      using namespace std;
+      vector<int> g1; 
+
+      for (int i = 1; i <= 5; i++) 
+          g1.push_back(i); 
+  
+      cout << "Output of begin and end: "; 
+      for (auto i = g1.begin(); i != g1.end(); ++i) 
+          cout << *i << " "; 
+  
+      cout << "\nOutput of cbegin and cend: "; 
+      for (auto i = g1.cbegin(); i != g1.cend(); ++i) 
+        cout << *i << " "; 
+  
+      cout << "\nOutput of rbegin and rend: "; 
+      for (auto ir = g1.rbegin(); ir != g1.rend(); ++ir) 
+          cout << *ir << " "; 
+  
+      cout << "\nOutput of crbegin and crend : "; 
+      for (auto ir = g1.crbegin(); ir != g1.crend(); ++ir) 
+          cout << *ir << " "; 
+          cout << "\n";
+      //return 0;
+}
+int main() {
+  function();
+
+  //int g2*; 
+  std::vector<int> *g2 = new std::vector<int>;
+  //g2.push_back(1);
+  //g2.push_back(2);
+  return g2[1];
+}
